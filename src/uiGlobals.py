7d69f9714fbb44778ee4df931f7a4d8dcac3f087
vinayn <vinayn@mcci.com>
2021-11-01 16:54:24 +0530

    fixes #32 README.md, VERSION.md, assets file

--- src/uiGlobals.py
+++ src/uiGlobals.py
@@ -28,7 +28,7 @@ import wx
 # GLOBAL VARIABLES
 ##############################################################################
 APP_NAME = "Cricket"
-APP_VERSION = "2.5.0_7"
+APP_VERSION = "2.5.0"
 
 # StatusBar ID
 SB_PORT_ID   = 0
