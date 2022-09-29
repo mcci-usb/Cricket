bcf0d7ba15c50ceff568c4d2f3f158ec7ff211b8
Vinay Namala <vinayn@mcci.com>
2022-09-29 16:20:53 +0530

    fix #83 #80 #81 Ubuntu20 and 22

--- src/uiGlobals.py
+++ src/uiGlobals.py
@@ -28,7 +28,7 @@ import wx
 # GLOBAL VARIABLES
 ##############################################################################
 APP_NAME = "Cricket"
-APP_VERSION = "2.6.1"
+APP_VERSION = "2.7.0"
 
 # StatusBar ID
 SB_PORT_ID   = 0
