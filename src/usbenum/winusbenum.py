6f9fc42dc2ba95430c1be9b76c1d0eb399a1dae8
vinaynmcci <vinayn@mcci.com>
2024-05-03 14:22:36 +0530

    Fix #33 added support for USB3 Tree view

--- src/usbenum/winusbenum.py
+++ src/usbenum/winusbenum.py
@@ -256,11 +256,13 @@ class WindowsUSBDeviceEnumerator(USBDeviceEnumerator):
         backend = None
         # Running Python-application on Windows
         if sys.platform == "win32":
-            backend = usb.backend.libusb1.get_backend(find_library=lambda x: "" + 
-                path + "Lib\\site-packages\\libusb\\_platform\\_windows\\x64\\libusb-1.0.dll")
+            backend = usb.backend.libusb1.get_backend()
 
         # Generator object
-        usb_devices = usb.core.find(find_all=True, backend=backend) 
+        usb_devices = usb.core.find(find_all=True, backend=backend)
+
+        # Generator object
+        # usb_devices = usb.core.find(find_all=True, backend=backend)
 
         # Here attached a list of Host controlloers, list of Hub,
         # List of periperals info with specific vid, pid.
