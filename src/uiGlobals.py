1aa593d227c1ef35c37328f43b80025410ad6f34
vinaynmcci <vinayn@mcci.com>
2025-03-19 13:09:50 +0530

    fix #145 Standardize Case for Labels

--- src/uiGlobals.py
+++ src/uiGlobals.py
@@ -223,7 +223,7 @@ portCnt = {"3141": 2,"3142":2, "3201": 4, "2301": 4, "2101": 1}
 ##############################################################################
 VERSION_NAME  = "\nMCCI"+u"\u00AE "+APP_NAME
 VERSION_ID    = ""
-VERSION_COPY  = "\nCopyright "+u"\u00A9"+" 2020-24\nMCCI Corporation"
+VERSION_COPY  = "\nCopyright "+u"\u00A9"+" 2020-25\nMCCI Corporation"
 VERSION_STR = "Version "+APP_VERSION
 
 
